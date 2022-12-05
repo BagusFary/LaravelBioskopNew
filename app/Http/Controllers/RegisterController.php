16becb65d6002d8cd1e13d0702e0f1fb1e8f9ebc
BagusFary <bagusfaryananta@gmail.com>
2022-12-05 09:50:46 +0700

    Event Listener

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Events\UserRegistered;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Session;
@@ -23,7 +22,7 @@ class RegisterController extends Controller
 
         $registerData = User::create($request->all());
 
-        event(new UserRegistered($registerData['email']));
+        event(new UserRegistered($request->input('email')));
         
         if($registerData){
             Session::flash('register-message', 'Register Success!');
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Events\UserRegistered;
+use App\Listeners\SendWelcomeEmail;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
